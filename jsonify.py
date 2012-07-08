d38f38c17d87e560c746df1cbd008bbde50cef2d
Ethan Sherbondy <sherbondye@gmail.com>
2012-07-08 15:42:46 -0700

    pepified

--- jsonify.py
+++ jsonify.py
@@ -1,55 +1,57 @@
 import re
 
+
 def cap_lower(name):
-	capname = name.capitalize()
-	lowername = name.lower()
-	# already camel-cased
-	if len(re.findall('[A-Z]', name)) > 0:
-		capname = name
-		lowername = lowername[0]+re.sub(r'([A-Z])',r'_\1',capname[1:]).lower()
-		lowername = re.sub("u_r_l", "url", lowername)
-	return (capname, lowername)
+    capname = name.capitalize()
+    lowername = name.lower()
+    # already camel-cased
+    if len(re.findall('[A-Z]', name)) > 0:
+        capname = name
+        lowername = lowername[0] + re.sub(r'([A-Z])', r'_\1', capname[1:]).lower()
+        lowername = re.sub("u_r_l", "url", lowername)
+    return (capname, lowername)
+
 
 def blockify(block, *args, **kwargs):
-	capblock, lowerblock = cap_lower(block)
-	print "{block:"+capblock+"}"
-	if len(args) == 0:
-		args = []
-	for name in args:
-		capname, lowername = cap_lower(name)
-		print "  "+lowername+": {JSPlaintext"+capname+"},"
-	for blockname in kwargs:
-		capname, lowername = cap_lower(blockname)
-		blockarg = kwargs[blockname]
-		print "  {block:"+capname+"}"
-		if isinstance(blockarg, list):
-			print "    "+lowername+": {"
-			for variant in blockarg:
-				variant = str(variant)
-				print "      "+variant+": {JSPlaintext"+capname+"-"+variant+"},"
-			print "    },"
-		else:
-			capvalue, lowervalue = cap_lower(blockarg)
-			print "    "+lowervalue+": {JSPlaintext"+capvalue+"},"
-		print "  {/block:"+capname+"}"
-	print "{/block:"+block+"}"
+    capblock, lowerblock = cap_lower(block)
+    print "{block:" + capblock + "}"
+    if len(args) == 0:
+        args = []
+    for name in args:
+        capname, lowername = cap_lower(name)
+        print "  " + lowername + ": {JSPlaintext" + capname + "}, "
+    for blockname in kwargs:
+        capname, lowername = cap_lower(blockname)
+        blockarg = kwargs[blockname]
+        print "  {block:" + capname + "}"
+        if isinstance(blockarg, list):
+            print "    " + lowername + ": {"
+            for variant in blockarg:
+                variant = str(variant)
+                print "      " + variant + ": {JSPlaintext" + capname + "-" + variant + "}, "
+                print "    }, "
+        else:
+            capvalue, lowervalue = cap_lower(blockarg)
+            print "    " + lowervalue + ": {JSPlaintext" + capvalue + "}, "
+        print "  {/block:" + capname + "}"
+    print "{/block:" + block + "}"
 
 
-sizes = [16,24,30,40,48,64,96,128]
+sizes = [16, 24, 30, 40, 48, 64, 96, 128]
 
 blockify("PortraitURL", PortraitURL=sizes)
 
-blockify("link","URL","name","target",description="description")
+blockify("link", "URL", "name", "target", description="description")
 
 blockify("chat", title="title")
 
 blockify("lines", "name", "line", "UserNumber", label="label")
 
-blockify("audio", PlayCount="playcount", AudioEmbed=[250,400,500,640], caption="caption", Artist="Artist", AlbumArt="AlbumArtURL", TrackName="TrackName", ExternalAudio="ExternalAudioURL")
+blockify("audio", PlayCount="playcount", AudioEmbed=[250, 400, 500, 640], caption="caption", Artist="Artist", AlbumArt="AlbumArtURL", TrackName="TrackName", ExternalAudio="ExternalAudioURL")
 
-blockify("ExternalAudio","ExternalAudioURL")
+blockify("ExternalAudio", "ExternalAudioURL")
 
-blockify("video", "PlayCount", "FormattedPlayCount", "PlayCountWithLabel", Video=[500,400,250], caption="caption")
+blockify("video", "PlayCount", "FormattedPlayCount", "PlayCountWithLabel", Video=[500, 400, 250], caption="caption")
 
 blockify("Answer", "question", "answer", "asker", AskerPortraitURL=sizes)
 
@@ -73,4 +75,4 @@ blockify("ReblogRoot", "ReblogRootName", "ReblogRootTitle", "ReblogRootURL", Reb
 
 blockify("ReblogParentPortraitURL", ReblogParentPortraitURL=sizes)
 
-blockify("PhotoURL",PhotoURL=[500,400,250,100,75])
+blockify("PhotoURL", PhotoURL=[500, 400, 250, 100, 75])
